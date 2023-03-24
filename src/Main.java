6c7392fb1238e211982fa5f265f51519ee36e11d
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-03-24 05:17:06 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,57 @@
 public class Main {
     public static void main(String[] args) {
         String login, password,  confirmPassword;
-        login = "abc_ABC";
-        password = "12345";
+        login = "11111111111111111111";
+        password = "dr_AAA";
         confirmPassword = password;
 
-        System.out.println("Значения параметров подходят под ограничения ? " +
+        System.out.println("Значения параметров подходят под ограничения? \n" +
                 checkLogin( login, password, confirmPassword));
     }
-    public static boolean checkLogin(String login, String password, String confirmPassword){
-        char[] log = login.toCharArray();
-        char[] pas = password.toCharArray();
-        char[] confPas = confirmPassword.toCharArray();
+    public static boolean checkLogin (String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException{
         boolean result = true;
-        if(log.length>20 || pas.length != confPas.length || pas.length>=20){
+        try {
+            char[] log = login.toCharArray();
+            if (log.length == 0) {
+                throw new NullPointerException("");
+            }
+            if (log.length > 20) {
+                throw new WrongLoginException("");
+            }
+            for (int i = 0; i < log.length; i++) {
+                if (!checkSymbol(log[i])) {
+                    return false;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Поле 'Login' не должно быть пустым.");
             return false;
+        } catch (WrongLoginException e) {
+            System.out.println("Число символов в поле 'Login' не должно превышать 20.");
+            return false;
+        }finally {
+            System.out.println("Проверка завершена");
         }
-        for (int i = 0; i < log.length; i++) {
-            if (!checkSymbol(log[i])) {
-                return false;
+        try {
+            char[] pas = password.toCharArray();
+            char[] confPas = confirmPassword.toCharArray();
+            if (pas.length == 0 || confPas.length == 0) {
+                throw new NullPointerException("");
             }
-        }
-        for (int i = 0; i < pas.length; i++) {
-            if (!checkSymbol(pas[i]) || pas[i] != confPas[i] ) {
-                return false;
+            if (pas.length != confPas.length || pas.length >= 20) {
+                throw new WrongPasswordException("");
             }
+            for (int i = 0; i < pas.length; i++) {
+                if (!checkSymbol(pas[i]) || pas[i] != confPas[i]) {
+                    return false;
+                }
+            }
+        }catch (NullPointerException e) {
+            System.out.println("Поля 'Password' и 'ConfirmPassword' не должны быть пустыми.");
+            return false;
+        } catch (WrongPasswordException e) {
+            System.out.println("Поля 'Password' и 'ConfirmPassword' должны совпадать и не превышать 19 символов.");
+            return false;
         }
         return result;
     }
